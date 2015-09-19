4afa212aabda7d66e8b5cc3150fce52ea42fedec
Robin Stephenson <robin.mascros@gmail.com>
2015-09-19 12:52:50 +0100

    Created unlay_all func which sets state of all edges to unplayed

--- MyPlayer/helper.py
+++ MyPlayer/helper.py
@@ -1,3 +1,10 @@
+import const
+
 def edge_setter(edges, state, board):
     for edge in edges:
         board.setEdgeState(edge, state)
+
+
+def unplay_all(board):
+    for edge in range(72):
+        board.setEdgeState(edge, const.UNPLAYED)
