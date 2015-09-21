51787d7bc61b720a9c5621e8721a1f2da02fe38f
Robin Stephenson <robin.mascros@gmail.com>
2015-09-21 18:45:01 +0100

    Skip unwritten tests, created no block test, rewrote some docstrings

--- MyPlayer/test/test_start_of_game_placer.py
+++ MyPlayer/test/test_start_of_game_placer.py
@@ -1,5 +1,7 @@
 import unittest
 import const
+from start_of_game_placer import StartOfGamePlacer
+placer = StartOfGamePlacer()
 
 
 class TestGetMove(unittest.TestCase):
@@ -7,26 +9,30 @@ class TestGetMove(unittest.TestCase):
         """
         should return each edge const.STARTERS, in order, if the opponent has not played any of those edges or any edges preventing us from doing so without giving the opponent a square
         """
-        # for edge in const.STARTERS:
-        #     self.assertEqual(edge, <PUT get_move CALL HERE>)
-        pass
+        # Convert to list so we can use .remove()
+        unplayed = list(range(72))
+        
+        # Get rid of some random non starter edges
+        unplayed.remove(1)
+        unplayed.remove(12)
+        unplayed.remove(50)
+
+        # Check it returns the right ones
+        for edge in const.STARTERS:
+            result = placer.get_move(unplayed)
+            unplayed.remove(result)
+            self.assertEqual(edge, result)
 
 
+    @unittest.skip("Not Started")
     def test_help(self):
         """
         should return edges in const.STARTERS, in order, skipping those the opponent has placed for us
         """
         pass
-        # get move could take a list of unplayed edges, we could fake this list to pretend the opponent has played one and check that it reacts accordingly
 
 
-    def test_block(self):
-        """
-        should return as many edges as possible from const.STARTERS, in order, missing those which would let the opponent create a square
-        """
-        pass
-
-
+    @unittest.skip("Not Started")
     def test_combo(self):
         """
         should return as many edges as possible from const.STARTERS, in order, missing those which would let the opponent create a square, skipping those that the opponent has placed for us
@@ -34,6 +40,7 @@ class TestGetMove(unittest.TestCase):
         pass
 
 
+    @unittest.skip("Not Started")
     def test_end(self):
         """
         should return None when all of const.STARTERS have been places, or can no longer be placed
@@ -41,6 +48,15 @@ class TestGetMove(unittest.TestCase):
         pass
 
 
+    @unittest.skip("Not Started")
+    def test_block(self):
+        """
+        should return as many edges as possible from const.STARTERS, in order, missing those which would let the opponent create a square
+        """
+        pass
+
+
+    @unittest.skip("Not Started")
     def test_never_returns_played(self):
         """
         should never return an edge which has already been played
